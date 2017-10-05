6d92d6d87be70b824897d458170ef8dcfe9a63b3
Andrew Walz <andrewjwalz@gmail.com>
2017-10-05 11:51:09 -0600

    Update

--- Stripe/STPCardIOProxy.m
+++ Stripe/STPCardIOProxy.m
@@ -56,6 +56,7 @@
 + (id)initWithPaymentDelegate:id;
 @property (nonatomic, assign, readwrite) BOOL hideCardIOLogo;
 @property (nonatomic, assign, readwrite) BOOL disableManualEntryButtons;
+@property (nonatomic, assign, readwrite) BOOL keepStatusBarStyleForCardIO;
 @property(nonatomic, retain, readwrite) UIColor *navigationBarTintColorForCardIO;
 @property (nonatomic, assign, readwrite) CGFloat scannedImageDuration;
 @end
@@ -70,6 +71,7 @@
     && [proxiedClass instancesRespondToSelector:@selector(initWithPaymentDelegate:)]
     && [proxiedClass instancesRespondToSelector:@selector(setHideCardIOLogo:)]
     && [proxiedClass instancesRespondToSelector:@selector(setDisableManualEntryButtons:)]
+    && [proxiedClass instancesRespondToSelector:@selector(setKeepStatusBarStyleForCardIO:)]
     && [proxiedClass instancesRespondToSelector:@selector(setNavigationBarTintColorForCardIO:)]
     && [proxiedClass instancesRespondToSelector:@selector(setScannedImageDuration:)];
 }
@@ -108,6 +110,7 @@
     cardIOViewController.hideCardIOLogo = YES;
     cardIOViewController.disableManualEntryButtons = YES;
     cardIOViewController.scannedImageDuration = 0;
+    cardIOViewController.keepStatusBarStyleForCardIO = NO;
     cardIOViewController.navigationBarTintColorForCardIO = [UIColor colorWithRed:234.0/255.0 green:88.0/255.0 blue:86.0/255.0 alpha:1.0];
     [viewController presentViewController:cardIOViewController animated:YES completion:nil];
 }
